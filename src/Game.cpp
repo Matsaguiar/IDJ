d6d976c4960c756f9d6957e7951459676d574b9f
matsaguiar <aguiar.matheuss@gmail.com>
2022-06-22 15:52:39 -0300

    pagina4OK

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,15 +1,23 @@
 #include "Game.hpp"
 
+Game* Game::instance = nullptr;
+
 Game::Game(string title, int width, int height){
     if(instance == nullptr){
         instance = this;
         
+        if(!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER))
+            cout << "Erro SDL_Init: " << SDL_GetError() << endl;
+        
+        if(!IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF))
+            cout << "Erro IMG_Init: " << SDL_GetError() << endl;
+        
+        if(!)
 
 
-    }else{
+    }else
         printf("\nInstancia já criada!\n");
-        exit(1);
-    }
+    
 }
 
 Game::~Game(){
